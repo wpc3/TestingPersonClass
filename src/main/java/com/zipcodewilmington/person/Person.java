22d4fb9a52a561c1f6b4c3b11e8995874d987485
Kristofer <kris@youngers.org>
2024-06-20 14:16:18 -0400

    Initial commit

---
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -0,0 +1,35 @@
+package com.zipcodewilmington.person;
+
+/**
+ * If you did ObjectsOne lab, you might recognize this code?
+ */
+public class Person {
+    private String name;
+    private int age;
+
+    public Person() {
+    }
+
+    public Person(int age) {
+    }
+
+    public Person(String name) {
+    }
+
+    public Person(String name, int age) {
+    }
+
+    public void setName(String name) {
+    }
+
+    public void setAge(int age) {
+    }
+
+    public String getName() {
+        return null;
+    }
+
+    public Integer getAge() {
+        return null;
+    }
+}
